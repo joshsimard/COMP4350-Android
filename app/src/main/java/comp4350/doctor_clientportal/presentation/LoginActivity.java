1a2af22f4a70d23e5dad52d42857d3fb6cb17668
edmondcotterell <edmondcotterell@gmail.com>
2016-03-17 16:11:18 -0500

    Improved vague implementation of api calls

--- app/src/main/java/comp4350/doctor_clientportal/presentation/LoginActivity.java
+++ app/src/main/java/comp4350/doctor_clientportal/presentation/LoginActivity.java
@@ -1,5 +1,6 @@
 package comp4350.doctor_clientportal.presentation;
 import comp4350.doctor_clientportal.R;
+import comp4350.doctor_clientportal.objects.EventApi;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -7,6 +8,7 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -22,8 +24,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
-import android.util.Base64;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -33,19 +33,11 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.ProtocolException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Handler;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
@@ -78,10 +70,15 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private View mLoginFormView;
     private Button button;
 
+    public final static String EXTRA_MESSAGE = "CalEvents";
+    public static String apiEvents = "";
+    private   android.os.Handler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
@@ -103,9 +100,24 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             @Override
             public void onClick(View view) {
                 //attemptLogin();
-                RetrieveFeedTask runner = new RetrieveFeedTask();
+
+
+                EventApi runner = new EventApi(getApplicationContext(), new android.os.Handler());
                 //String sleepTime = time.getText().toString();
                 runner.execute();
+
+//                final String[] message = {""};
+//                handler = new android.os.Handler() {
+//                    @Override
+//                    public void handleMessage(Message msg) {
+//                        message[0] = msg.getData().getString("file"); // You can change this according to your requirement.
+//
+//                    }
+//                };
+                //Intent intent = getIntent();
+                //String message = apiEvents; //intent.getStringExtra(LoginActivity.EXTRA_MESSAGE);
+                //handler.handleMessage(runner.handler.obtainMessage());
+                //Toast.makeText(LoginActivity.this,message[0], Toast.LENGTH_LONG).show();
             }
         });
 
@@ -393,64 +405,5 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         }
     }
 
-
-    private class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
-
-        private Exception exception;
-
-        public final static String apiURL = "http://ec2-52-32-93-246.us-west-2.compute.amazonaws.com/api/";
-
-        protected void onPreExecute() {
-            //progressBar.setVisibility(View.VISIBLE);
-            //responseView.setText("");
-        }
-
-        protected String doInBackground(Void... urls) {
-
-            HttpURLConnection urlConnection = null;
-            // Do some validation here
-            try {
-                URL url = new URL(apiURL + "events");
-                String auth = android.util.Base64.encodeToString(
-                        ("john@doe.com" + ":" + "password").getBytes(),
-                        android.util.Base64.NO_WRAP
-                );
-
-                try {
-                    urlConnection = (HttpURLConnection) url.openConnection();
-                    urlConnection.setRequestMethod("GET");
-                    urlConnection.addRequestProperty("Authorization", "Basic " + auth);
-                    urlConnection.connect();
-                    InputStream stream = urlConnection.getInputStream();
-
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
-                    StringBuilder stringBuilder = new StringBuilder();
-
-                    String line;
-                    while ((line = bufferedReader.readLine()) != null) {
-                        stringBuilder.append(line).append("\n");
-                    }
-                    bufferedReader.close();
-                    return stringBuilder.toString();
-                }
-                finally{
-                    urlConnection.disconnect();
-                }
-            }
-            catch(Exception e) {
-                Log.e("ERROR", e.getMessage(), e);
-                return null;
-            }
-        }
-
-        protected void onPostExecute(String response) {
-            if(response == null) {
-                response = "THERE WAS AN ERROR";
-            }
-            //progressBar.setVisibility(View.GONE);
-            Log.i("INFO", response);
-            //responseView.setText(response);
-        }
-    }
 }
 

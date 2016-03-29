9e8ecf8a63ee925caf01407d671225f37a86fa57
John <johnlarmie@gmail.com>
2016-03-29 17:40:53 -0500

    Added volley connection and input validation

--- app/src/main/java/comp4350/doctor_clientportal/presentation/RegisterActivity.java
+++ app/src/main/java/comp4350/doctor_clientportal/presentation/RegisterActivity.java
@@ -8,10 +8,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
 import comp4350.doctor_clientportal.R;
+import comp4350.doctor_clientportal.objects.Client;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -21,6 +35,8 @@ public class RegisterActivity extends AppCompatActivity {
     TextView password_view;
     TextView passwword_cfm_view;
     Button register_button;
+
+    public final static String apiURL = "http://ec2-52-32-93-246.us-west-2.compute.amazonaws.com/api/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,9 +61,95 @@ public class RegisterActivity extends AppCompatActivity {
         register_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //DO SHIT
+
+
+                if (!(password_view.getText().toString().equalsIgnoreCase(passwword_cfm_view.getText().toString()))) {
+                    Toast.makeText(RegisterActivity.this, "Password Mismatch", Toast.LENGTH_LONG).show();
+                } else if (attemptRegister()) {
+                    //create request queue
+                    final RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+
+                    JSONObject postData = new JSONObject();
+                    JSONObject data = new JSONObject();
+
+                    try {
+                        postData.put("firstName", fName_view.getText().toString());
+                        postData.put("lastName", lName_view.getText().toString());
+                        postData.put("email", email_view.getText().toString());
+                        postData.put("password", password_view.getText().toString());
+
+                        data.put("data", postData);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+
+                    JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                            (Request.Method.POST, apiURL + "register", data, new Response.Listener<JSONObject>() {
+
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    try {
+                                        JSONArray jsonArray = response.getJSONArray("data");
+
+                                        Toast.makeText(RegisterActivity.this, jsonArray.getString(0), Toast.LENGTH_LONG).show();
+
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }, new Response.ErrorListener() {
+
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    // TODO Auto-generated method stub
+                                    //uiUpdate.setText("Response: " + error.toString());
+                                    Toast.makeText(RegisterActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                                }
+                            });
+                    // Add the request to the RequestQueue.
+                    queue.add(jsObjRequest);
+                }
             }
         });
+    }
+
+    private boolean attemptRegister()
+    {
+        boolean attempt = true;
+        if (fName_view.getText().toString().isEmpty())
+        {
+            fName_view.setError(getString(R.string.error_field_required));
+            attempt = false;
+        }
+        if (lName_view.getText().toString().isEmpty())
+        {
+            lName_view.setError(getString(R.string.error_field_required));
+            attempt = false;
+        }
+        if (email_view.getText().toString().isEmpty())
+        {
+            email_view.setError(getString(R.string.error_field_required));
+            attempt = false;
+        }
+        if (password_view.getText().toString().isEmpty())
+        {
+            password_view.setError(getString(R.string.error_field_required));
+            attempt = false;
+        }
+        if (passwword_cfm_view.getText().toString().isEmpty())
+        {
+            passwword_cfm_view.setError(getString(R.string.error_field_required));
+            attempt = false;
+        }
+
+        // Check for a valid email address.
+        if (!(email_view.getText().toString().contains("@")))
+        {
+            email_view.setError(getString(R.string.error_invalid_email));
+            attempt = false;
+        }
+        return attempt;
     }
 
 

cf769418d84a6b06060625705ad7b1ea95ee1641
Josh <joshuasimard@gmail.com>
2016-04-05 01:35:42 -0500

    - worked on robotium.     - not finished, but committing from my home pc so I can work on laptop

--- app/src/androidTest/java/comp4350/doctor_clientportal/presentation/RobotiumTest.java
+++ app/src/androidTest/java/comp4350/doctor_clientportal/presentation/RobotiumTest.java
@@ -1,6 +1,7 @@
 package comp4350.doctor_clientportal.presentation;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.Display;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 
@@ -35,6 +36,12 @@ public class RobotiumTest extends ActivityInstrumentationTestCase2<LoginActivity
 
         //click login
         solo.clickOnButton("Sign In");
+
+
+
+
+
+
 
         //select button VIEWCLIENT
         solo.clickOnButton("View Client");
@@ -105,8 +112,38 @@ public class RobotiumTest extends ActivityInstrumentationTestCase2<LoginActivity
         //click login
         solo.clickOnButton("Sign In");
 
-        //select button EDIT INFORMATION
-        solo.clickOnButton("Edit Information");
+        /* ***************************************
+         *          Appointments Tests
+         * ***************************************/
+
+        //verify on appointments page
+        solo.waitForActivity(CalanderActivity.class, 2000);
+        solo.assertCurrentActivity("wrong activity", CalanderActivity.class);
+
+        //find Checkup - Jane Doe
+        solo.searchText("Checkup - Jane Doe");
+
+        //click add button
+
+        //get title
+
+        //get date
+
+        //get time
+
+        //click Submit
+
+        //verify added thing there
+
+        /* ***************************************
+         *         Edit Information Tests
+         * ***************************************/
+
+        //click drawer
+        swipeToRight();
+
+        //click Edit Information
+        solo.clickOnText("Edit Information");
 
         //assert activity is Edit Client Activity
         solo.waitForActivity(EditClientActivity.class, 2000);
@@ -116,21 +153,100 @@ public class RobotiumTest extends ActivityInstrumentationTestCase2<LoginActivity
         solo.searchText("Jane");
         solo.searchText("Doe");
 
-        //back to client DoctorActivity
-        solo.goBack();
+        //add a phone number
+        solo.enterText((EditText) solo.getView(R.id.client_phone_edit), "(204)2039-283");
 
-        //select button EDIT INFORMATION
-        solo.clickOnButton("Appointments");
+        //click Save
+        solo.clickOnButton("Save");
 
-        //assert activity is Edit Client Activity
         solo.waitForActivity(CalanderActivity.class, 2000);
-        solo.assertCurrentActivity("wrong activity", CalanderActivity.class);
 
-        //search for the text on screen
-        solo.searchText("Checkup - Jane Doe");
+        //click drawer
+        swipeToRight();
 
-        //go back to client DoctorActivity
-        solo.goBack();
+        //click Edit Information
+        solo.clickOnText("Edit Information");
+
+        //check to see new phone number
+        solo.searchText("(204)2039-283");
+
+        /* ***************************************
+         *          Medical Terms Tests
+         * ***************************************/
+
+        //click drawer
+        swipeToRight();
+
+        //click Medical Terms
+        solo.clickOnText("Medical Terms");
+
+        //verify medical terms
+        solo.waitForActivity(MedicalTermsActivity.class, 2000);
+        solo.assertCurrentActivity("wrong activity", MedicalTermsActivity.class);
+
+        //find Morphine
+        solo.searchText("Morphine");
+
+        /* ***************************************
+         *          Requests Tests
+         * ***************************************/
+
+        //click drawer
+        swipeToRight();
+
+        //click Requests
+        solo.clickOnText("Requests");
+
+        //verify requests
+        solo.waitForActivity(ClientRequestActivity.class, 2000);
+        solo.assertCurrentActivity("wrong activity", ClientRequestActivity.class);
+
+        //find Nigerian puss
+        solo.searchText("Nigerian puss");
+
+        //find Declined
+        solo.searchText("Declined");
+
+        //click note button
+        solo.clickOnButton(R.id.edit_request_cl);
+
+        //find Your problem is beyond me. ..I can't help you
+        solo.searchText("Your problem is beyond me. ..I can't help you");
+
+        //click Ok
+        solo.clickOnButton("Ok");
+
+        //click add button
+        solo.clickOnButton(R.id.fab_request);
+
+        //verify AddRequestActivity
+        solo.waitForActivity(AddRequestActivity.class, 2000);
+        solo.assertCurrentActivity("wrong activity", AddRequestActivity.class);
+
+        //get Drug Name Weed
+
+        //get quantity 10
+
+        //click submit
+        solo.clickOnButton("Submit");
+
+        //find Pending
+        solo.searchText("Pending");
+
+        //click drawer
+        swipeToRight();
+
+        //click Logout
+        solo.clickOnButton("Logout");
+    }
+
+    private void swipeToRight() {
+        Display display = solo.getCurrentActivity().getWindowManager().getDefaultDisplay();
+        int width = display.getWidth();
+        int height = display.getHeight();
+        float xStart = 0 ;
+        float xEnd = width / 2;
+        solo.drag(xStart, xEnd, height / 2, height / 2, 1);
     }
 
     public void setUp() throws Exception {
